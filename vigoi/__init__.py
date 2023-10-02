fda6a482a02489db5e4ddb25d020cb293c2320a7
Yu-Chang YANG <yang.yc.allium@gmail.com>
2023-10-02 17:57:06 +0800

    refactor: wrap configurations

--- vigoi/__init__.py
+++ vigoi/__init__.py
@@ -7,24 +7,35 @@ import datetime
 import os
 
 from vigoi.new import newbookat
+from vigoi.check import checkbookat
 
-path = os.path.abspath(os.getcwd())
-inipath = os.path.join(path, "vigoi.ini")
-config = configparser.ConfigParser()
-config.read(inipath, encoding="utf-8")
-
-def findbook(date):
+def configure(filename="vigoi.ini"):
+	inipath = os.path.join(os.path.abspath(os.getcwd()), filename)
+	config = configparser.ConfigParser()
+	config.read(inipath, encoding="utf-8")
 	if not config.has_section("PATH"):
 		config.add_section("PATH")
-	if not config.has_option("PATH", "format"):
-		config.set("PATH", "format", "%%Y%%m_Accounts.yaml")
-	fnfmt = config.get("PATH", "format")
-	return date.strftime(fnfmt)
+	if not config.has_option("PATH", "book"):
+		config.set("PATH", "book", "%%Y%%m_Accounts.yaml")
+	if not config.has_option("PATH", "report"):
+		config.set("PATH", "report", "")
+	if not config.has_section("ACCOUNT"):
+		config.add_section("ACCOUNT")
+	if not config.has_option("ACCOUNT", "fakes"):
+		config.set("ACCOUNT", "fakes", "[]")
+	with codecs.open(inipath, "w") as fconfig:
+		config.write(fconfig)
+	return config
 
-def newbook(date = datetime.date.today() + datetime.timedelta(days=3)):
-	return newbookat(findbook(date), date)
+def nextdays(days=0):
+	return datetime.date.today() + datetime.timedelta(days=days)
+
+def newbook(date, config):
+	bookpath = date.strftime(config.get("PATH", "book"))
+	return newbookat(bookpath, date)
 
 def main():
+	config = configure()
 	parser = argparse.ArgumentParser(
 		description = "Tools for creating or checking accounting books.", 
 		usage = "vigoi [-h] [-n [YEAR MONTH]] [-x [YEAR MONTH]]")
@@ -39,7 +50,7 @@ def main():
 	if not args.new is None:
 		nargs = len(args.new)
 		if nargs == 0:
-			newbook()
+			newbook(nextdays(3), config)
 		elif nargs == 2:
 			try:
 				y = int(args.new[0])
@@ -47,8 +58,6 @@ def main():
 			except ValueError:
 				raise TypeError(
 					"Arguments following `-n` or `--new` must be integers!")
-			newbook(datetime.date(y, m, 15))
+			newbook(datetime.date(y, m, 15), config)
 		else:
 			parser.print_help()
-	with codecs.open(inipath, "w") as configfile:
-		config.write(configfile)
